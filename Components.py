9a9c6d6692a5e6653036c1cf14e3b01c86e21602
roliauo <roliauo@gmail.com>
2021-01-10 22:35:53 +0800

    [Fix] filter and groupby bug

--- Components.py
+++ Components.py
@@ -11,37 +11,57 @@ import constants
 plt.rcParams['font.sans-serif'] = ['Microsoft JhengHei', 'sans-serif'] 
 plt.rcParams['axes.unicode_minus'] = False
 
-class ControlPanel(tkinter.Frame):   
+class GroupbyPanel(tkinter.Frame):
+    def __init__(self, parent):
+        tkinter.Frame.__init__(self, parent) # 建立框架
+        self.pack(side=tkinter.TOP, fill='x') # 自動填滿左右(x方向)
+        # self.grid(row=0, sticky='n') #sticky 可以使用 n, e, s, w 及組合來定位
+        self.display(parent)
+
+    def display(self, parent):
+        def createCheckButton(rowId: int, itemName: str, checked: tkinter.StringVar):
+            if len(checked.get()) == 0:
+                checked.set('') # 預設 
+            # checkboxState = 'active' #if enabled else 'disabled'
+            checkbox = tkinter.Checkbutton(self, text=itemName, var=checked, onvalue=itemName, offvalue='', command=parent.onUpdateData) # onvalue/offvalue 設定 var 所得到的值  #, state=checkboxState
+            checkbox.grid(row=rowId)
+                       
+        ### checkbox (for groupby)
+        createCheckButton(3, '學校名稱', parent.prop_grouby[0])
+        createCheckButton(4, '科系名稱', parent.prop_grouby[1])
+        createCheckButton(5, '學位', parent.prop_grouby[2])
+
+        
+class FilterPanel(tkinter.Frame):   
     def __init__(self, parent):
         tkinter.Frame.__init__(self, parent) # 建立框架
         self.pack(fill='x') # 自動填滿左右(x方向)
-        self.createWidgets(parent) # 布置組件內容
+        # self.grid(row=1, sticky='n')
+        self.display(parent)
 
-    def createWidgets(self, parent):     
-        ### option list # or search?
-        def createOptionMenu(id: int, columnName: str, selection: tkinter.StringVar, label: str = ''):
-            labelTxt = columnName
-            if len(label) > 0:
-                labelTxt = label
-            optionLabel = tkinter.Label(self, text=labelTxt)
-            optionLabel.grid(row=id, column=0, padx=20, pady=10)
+    def display(self, parent):   
 
-            optionsList = parent.prop_dataFrame[columnName].unique().tolist() # pandas找出欄位:<學校名稱>所有不重複的值 (為numpy.ndarray，再轉為串列)  
+        def createOptionMenu(rowId: int, columnName: str, selection: tkinter.StringVar):
+            noColumn = columnName not in parent.prop_currentData_columns.get() # 判斷欄位是否在目前資料中
+            optionsList = ['-'] if noColumn else parent.prop_currentData[columnName].unique().tolist() # pandas找出欄位:<學校名稱>所有不重複的值 (為numpy.ndarray，再轉為串列)  
             optionsList.insert(0, constants.SELECTION_ALL) # 加入'全部'之選項
-
-            selection.set(optionsList[0]) # 預設選項一        
-            selection.trace('w', onUpdateData) # 監聽選項改變時，觸發function。 'w'為監聽模式
+            
+            if len(selection.get()) == 0:
+                selection.set(optionsList[0]) # 預設選項一  
 
             optionMenu = tkinter.OptionMenu(self, selection, *optionsList)
-            optionMenu.grid(row=id, column=1)
+            
+            if noColumn: # 若目前資料沒有此欄位的話禁止點選              
+                optionMenu.configure(state='disabled')
 
-        def onUpdateData(*args): # *args 為不限定長度之參數 print(args) -> output: ('PY_VAR0', '', 'w')
-            parent.updateFrame(ReportPanel) # 更新ReportPanel的資料
+            optionLabel = tkinter.Label(self, text=columnName) # 建立label
+            optionLabel.grid(row=rowId, column=0, padx=20, pady=5) # 佈件
+            optionMenu.grid(row=rowId, column=1, padx=20, pady=5) 
 
         ### option menu
         createOptionMenu(0, '學校名稱', parent.prop_school)
         createOptionMenu(1, '科系名稱', parent.prop_department)
-        createOptionMenu(2, '等級別', parent.prop_degree, '學位')
+        createOptionMenu(2, '學位', parent.prop_degree)
 
         ### chart button
         def showChart(dataFrame, chartType: str, columnX: str, columnY: str):
@@ -64,21 +84,18 @@ class ControlPanel(tkinter.Frame):
             plt.show()
 
         btn_showChart = tkinter.Button(self, text='Chart', command=show)
-        btn_showChart.grid(row=3)
+        btn_showChart.grid(row=6, columnspan=3)
+
 
 class ReportPanel(tkinter.Frame):
     def __init__(self, parent):
         tkinter.Frame.__init__(self, parent) 
-        self.pack(fill='both', expand=True) # 填滿全部
-        self.createWidgets(parent) # 布置組件內容
+        self.pack(side=tkinter.BOTTOM,fill='both', expand=True) # 填滿全部
+        # self.grid(row=2, sticky='nesw')
+        self.display(parent) # 布置組件內容
 
-    def createWidgets(self, parent):  
-        # print('ReportPanel:', parent.prop_school.get())
-        data = parent.filterData()
-
-        report = Table(self, dataframe=data, showstatusbar=True,  editable=False)         
-        
+    def display(self, parent):  
+        data = parent.prop_currentData #parent.getData()
+        report = Table(self, dataframe=data, showstatusbar=True, editable=False) # 建立table               
         report.showIndex() # 加入此行才能顯示index
-        report.adjustColumnWidths()
         report.show()
-
--- app.py
+++ app.py
@@ -6,7 +6,8 @@ import pandas # 資料處理套件
 import re # 字串處理
 import csv
 import constants # 自定義常數
-from components import ControlPanel
+from components import GroupbyPanel
+from components import FilterPanel
 from components import ReportPanel
 
 class App(tkinter.Tk):
@@ -16,65 +17,80 @@ class App(tkinter.Tk):
         ### window setting
         self.title('Python 期末專題 - 507170055 林宥如')
         self.geometry(str(constants.WINDOW_WIDTH) + 'x' + str(constants.WINDOW_HEIGHT)) # 設定視窗大小(參數為字串)
-        self.configure(background='white') # background
+        self.minsize(constants.WINDOW_WIDTH, constants.WINDOW_HEIGHT)
+        # self.configure(background='white') # background
+        # tkinter.Grid.rowconfigure(self, 0, weight=1) # y方向拓展
+        # tkinter.Grid.columnconfigure(self, 0, weight=1) # x方向拓展
 
         ### properties setting
-        self.prop_dataFrame = None # 原始資料表
-        self.prop_columnsDict = {} # 資料欄位字典
-        self.prop_school = tkinter.StringVar() # tkinter.StringVar()為tkinter的追蹤變數，取值用get()
+        self.prop_originalData = None # 原始資料表
+        self.prop_currentData = None # 目前顯示之資料
+        self.prop_currentData_columns = tkinter.StringVar() # 追蹤目前資料欄位
+        self.prop_school = tkinter.StringVar() # tkinter.StringVar()為tkinter的追蹤變數，預設為空字串:''，取值用get()
         self.prop_degree = tkinter.StringVar() # 學位
         self.prop_department = tkinter.StringVar() # 科系
-        self.prop_grouby = []
+        self.prop_grouby = [tkinter.StringVar() for i in range(3)] # 建立一個長度為3 的 StringVar串列 (因會放入學校/科系/學位，這三個項目)
         self.frames = {} # 存取框架
 
-        ### display
         self.loadData()
-        self.createWidgets() # 布置組件內容
+        self.display() # 布置組件內容      
+
+        ### 監聽目前資料欄位，監聽選項改變時，觸發function。 'w'為監聽模式
+        def updateFilterPanel(*args):
+            self.updateFrame(FilterPanel)
+        self.prop_currentData_columns.trace('w', updateFilterPanel)
+        self.prop_school.trace('w', self.onUpdateData) 
+        self.prop_degree.trace('w', self.onUpdateData) 
+        self.prop_department.trace('w', self.onUpdateData) 
+
+
+    def onUpdateData(self, *args): # *args 為不限定長度之參數 print(args) -> output: ('PY_VAR0', '', 'w')
+        self.updateFrame(GroupbyPanel) # 因為布局關係所以更新 (原本不加的話用pack佈局會錯位)
+        self.updateFrame(FilterPanel)
+        self.updateData()
+        self.updateFrame(ReportPanel) # 更新ReportPanel的資料
     
     def updateFrame(self, frameClass): 
-        frame = frameClass(self) # 建立
-        if self.frames[frameClass] is not None:
-            self.frames[frameClass].destroy() # 若框架存在的話即銷毀
+        frame = frameClass(self) # 建立框架
+        if self.frames[frameClass] is not None: # 若框架存在的話即銷毀
+            self.frames[frameClass].destroy() 
         self.frames[frameClass] = frame
     
     def loadData(self):
         dataUrl = 'http://stats.moe.gov.tw/files/detail/108/108_students.csv'
-        self.prop_dataFrame = pandas.read_csv(dataUrl) # pandas獲取資料輸出為dataFrame
-
-        columns = self.prop_dataFrame.columns.values.tolist() # 得到資料表的欄位串列
-        zipColumns = zip(columns, columns)
-        self.prop_columnsDict = dict(zipColumns) # 將欄位轉成字典表
-        # print(self.prop_columnsDict)
+        self.prop_originalData = pandas.read_csv(dataUrl) # pandas獲取資料輸出為dataFrame
+        # merge: pandas.concat([df1, df2], axis=)
 
         ### parse data
+        self.prop_originalData = self.prop_originalData.rename({'等級別': '學位'}, axis='columns') # 更改欄位名稱
+        columns = self.prop_originalData.columns.values.tolist() # 得到資料表的欄位串列
+      
         # 將計數欄位轉成數字
-        data = self.prop_dataFrame.copy()
+        data = self.prop_originalData.copy()
         for col in columns:
             if len(re.findall("計|男|女", col)) > 0:
                 data[col] = [int(re.sub(',|-', '', x)) if len(re.sub(',|-', '', x)) > 0 else 0 for x in data[col].tolist()] # 欄位值字串轉數字 (方法一) : re.sub取代記號後轉數字
                 # data[col] = data[col].apply(pandas.to_numeric, errors='coerce') # 欄位值字串轉數字 (方法二: 套件函式)，errors='coerce'為非數字用NaN取代 
-        self.prop_dataFrame = data
-        # merge: pandas.concat([df1, df2], axis=)
+                
+        data['學校代碼'] = data['學校代碼'].astype(str) # 數字轉為字串 (因groupby 算總和時會相加失真)
+
+        self.prop_originalData = data
+        self.prop_currentData = data
+        self.prop_currentData_columns.set(self.prop_originalData.columns.values)
 
     def dataAnalysis(self):
-        # print(self.prop_dataFrame.groupby('學校名稱').get_group('國立政治大學'))
-        data = self.prop_dataFrame.copy()
+        # print(self.prop_originalData.groupby('學校名稱').get_group('國立政治大學'))
+        data = self.prop_originalData.copy()
         columns = data.columns.values.tolist() # 得到資料表的欄位串列
-
-        # ironman_df = pandas.DataFrame({
-        #     'team':['A','B','C','C','B','B'],
-        #     'number':[int(x) for x in list(['7', '8', '7', '0', '8', '1'])] # 將字串轉成數字串列
-        #     }, #np.random.randint(10, size=6) #['7', '8', '7', '0', '8', '1']
-        #     columns=['team','number'])
-        # print(ironman_df)
-        # print(ironman_df.groupby('team').sum()) #
 
         ##### calculate
         # allStudentsNum = data['總計'].sum().astype(int) # 總就學人數
 
         ##### new data       
         # data.groupby(['學校代碼', '學校名稱']).sum().astype(int) # 校別總人數  # sum有小數點，用astype轉換為int
-        # data = data.groupby(['學校代碼', '學校名稱', '科系名稱'], as_index=False).sum() # 校+系別
+        # data = data.groupby(['學校代碼', '學校名稱', '科系名稱'], as_index=False).sum() # 校+系別            
+        # data = data.groupby(['學校代碼', '學校名稱', '科系名稱'], as_index=False).sum()  #, as_index=False
+        # data = data.groupby(['學校代碼', '學校名稱', '科系名稱', '學位'], as_index=False).sum() 
 
         ### as_index=False 設定非索引需指定欄位轉成數字，不可一同轉換
         # data.groupby(['學校代碼', '學校名稱'], as_index=False).sum()
@@ -88,44 +104,60 @@ class App(tkinter.Tk):
 
         # })
 
-    def groupbyData(self):
-        data = self.prop_dataFrame.copy() # default data
+    def updateData(self):
+        data = self.prop_originalData.copy() # use original data
+        data = self.groupbyData(data)
+        data = self.filterData(data)
+        self.prop_currentData = data # set current data
+        self.prop_currentData_columns.set(self.prop_currentData.columns.values)
 
-        if len(self.prop_grouby) > 0:
-            data = data.groupby(self.prop_grouby, as_index=False).sum() 
+    def groupbyData(self, varData):
+        data = varData.copy() # default data
+        groupbyList = [x.get() for x in self.prop_grouby if len(x.get()) > 0] # 產生prop_grouby的字串串列
+        # groupbyList = [column for column in groupbyList if column in self.prop_currentData_columns.get()] # 目前資料欄位出現的才做groupby
+
+        if '學校名稱' in groupbyList:
+            groupbyList.insert(0, '學校代碼')
+
+        if len(groupbyList) > 0:
+            data = data.groupby(groupbyList, as_index=False).sum() 
         
         return data
-        
     
-    def filterData(self):
-        data = self.prop_dataFrame.copy() # default data
+    def hasFilter(self, condition: tkinter.StringVar): # 判斷是否有此條件 (若選擇'全部'視為無條件)
+        if condition is None or len(condition.get()) < 1 or condition.get().find(constants.SELECTION_ALL) > -1 :
+            return False
+        else:
+            return True
+           
+    def filterData(self, varData):
+        data = varData.copy() # default data
 
-        # 判斷是否有此條件
-        hasFilter: bool = lambda condition: False if condition is None or len(condition.get()) < 1 or condition.get().find(constants.SELECTION_ALL) > -1 else True 
-        
-        # data = data.groupby(['學校代碼', '學校名稱', '科系名稱'], as_index=False).sum()  #, as_index=False
-        # data = data.groupby(['學校代碼', '學校名稱', '科系名稱', '等級別'], as_index=False).sum() 
-        
+        hasColumn: bool = lambda columnName:  columnName in data.columns.values.tolist() # 判斷欄位是否在目前資料中        
         # pandas: []可過濾資料，當存在條件時過濾資料，因為會有多重條件且不一定同時存在，所以不能同時過濾      
-        if hasFilter(self.prop_school):
+        if hasColumn('學校名稱') and self.hasFilter(self.prop_school):
             data = data[data['學校名稱'] == self.prop_school.get()] 
         
-        if hasFilter(self.prop_department):
+        if hasColumn('科系名稱') and self.hasFilter(self.prop_department):
             data = data[data['科系名稱'] == self.prop_department.get()]
         
-        if hasFilter(self.prop_degree):
-            data = data[data['等級別'] == self.prop_degree.get()]
+        if hasColumn('學位') and self.hasFilter(self.prop_degree):
+            data = data[data['學位'] == self.prop_degree.get()]
         
         return data
         
-    def createWidgets(self):      
-        controlPanel = ControlPanel(self)
+    def display(self):  
+        groupbyPanel = GroupbyPanel(self)    
+        filterPanel = FilterPanel(self)
         reportPanel = ReportPanel(self)
 
-        self.frames[ControlPanel] = controlPanel # set frames
+        self.frames[GroupbyPanel] = groupbyPanel # set frames
+        self.frames[FilterPanel] = filterPanel 
         self.frames[ReportPanel] = reportPanel 
     
 
 if __name__ == "__main__":
     app = App()
-    app.mainloop()
+    app.mainloop() # 視窗持續存在
+
+#133-66=67
